9637fe19d61ac250a4ecd55e812c9ed241d852de
unknown <pranav.sinha17@gmail.com>
2020-04-17 18:50:35 +0530

    Project initiated

---
+++ src/main/java/com/cg/pecuniabank/PecuniBankRestServicesApplication.java
@@ -0,0 +1,13 @@
+package com.cg.pecuniabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PecuniBankRestServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PecuniBankRestServicesApplication.class, args);
+	}
+
+}
